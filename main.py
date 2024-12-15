d73d47a37d171e2e50de0cbc99bc7ab4d9ef5dfd
Kate <e.a.vikulova@yandex.ru>
2024-12-15 10:50:35 +0300

    Adding files

---
+++ main.py
@@ -0,0 +1,18 @@
+print('Hello,hello,hello')
+print('1')
+print('2')
+print('3')
+
+Completed_tasks=12
+Hours_total=1.5
+Title='Python'
+Time_to_one_task=Hours_total/Completed_tasks
+print('Курс:',Title,',всего задач:',Completed_tasks,',затрачено часов:',Hours_total,',среднее время выполнения:',Time_to_one_task)
+
+example='Поросёночек'
+print(example[0])
+print(example[-1])
+print(len(example))
+print(example[5:11])
+print(example[-1::-1])
+print(example[1:11:2])
---
+++ module_1_3.py
@@ -0,0 +1,8 @@
+Name='Kate'
+print('Name:',Name)
+Age=38
+print('Age:',Age)
+New_age=Age+1
+print('Correct_Age:',New_age)
+Is_student= True
+print('is_a_student:',Is_student)
