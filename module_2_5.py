0d1c6954eb2dbeac9b0a845c63f2441dbfa0ab5a
Kate <e.a.vikulova@yandex.ru>
2024-12-17 10:47:57 +0300

    Matrix

--- module_2_5.py
+++ module_2_5.py
@@ -2,7 +2,7 @@ def get_matrix(n, m, value):
     matrix = []
     for i in range(n):
         matrix.append([])
-        for j in range(m):
+        for k in range(m):
             matrix[i].append(value)
     return matrix
 result1 = get_matrix(2, 2, 10)
